ba765b1429c20a7681d5f08bb960a080a5d7ab53
mistople <daviddossett@outlook.com>
2020-12-20 22:56:31 -0800

    Nav improvements

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -3,20 +3,28 @@ import './nav.css';
 import logo from '../../images/logo.svg';
 import { NavLink } from 'react-router-dom';
 
+function StyledNavItem({ children, to }) {
+	return (
+		<NavLink to={to} exact={true} activeClassName={'activeNavItem'}>
+			{children}
+		</NavLink>
+	);
+}
+
 export function Nav({ className }) {
 	return (
 		<nav className={`${className} nav`}>
-			<NavLink to="/">
-				<img src={logo} alt="Formula 1 logo" />
-			</NavLink>
-			<NavLink to="/1950">1950</NavLink>
-			<NavLink to="/1960">1960</NavLink>
-			<NavLink to="/1970">1970</NavLink>
-			<NavLink to="/1980">1980</NavLink>
-			<NavLink to="/1990">1990</NavLink>
-			<NavLink to="/2000">2000</NavLink>
-			<NavLink to="/2010">2010</NavLink>
-			<NavLink to="/2020">2020</NavLink>
+			<StyledNavItem to={'/'}>
+				<img src={logo} alt="Logo" />
+			</StyledNavItem>
+			<StyledNavItem to={'/1950'}>1950</StyledNavItem>
+			<StyledNavItem to={'/1960'}>1950</StyledNavItem>
+			<StyledNavItem to={'/1970'}>1950</StyledNavItem>
+			<StyledNavItem to={'/1980'}>1950</StyledNavItem>
+			<StyledNavItem to={'/1990'}>1950</StyledNavItem>
+			<StyledNavItem to={'/2000'}>1950</StyledNavItem>
+			<StyledNavItem to={'/2010'}>1950</StyledNavItem>
+			<StyledNavItem to={'/2020'}>1950</StyledNavItem>
 		</nav>
 	);
 }
